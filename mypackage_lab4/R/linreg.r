460d4d28f8a87cdb944f6681e9d50ac41940c4d1
xuyixuan <xuyixuan@users.noreply.github.com>
2015-09-23 01:31:05 +0200

    every thing works now

--- mypackage_lab4/R/linreg.r
+++ mypackage_lab4/R/linreg.r
@@ -19,9 +19,7 @@
 linreg <- setRefClass("linreg",
                       fields=list(formula="formula", 
                                   data = "data.frame", 
-                                  #X = "matrix",
-                                  #y = "matrix",
-                                  bhat = "matrix"
+                                  bhat = "matrix",
                                   resid = "matrix",
                                   yhat = "matrix",
                                   df = "integer",
@@ -29,84 +27,67 @@ linreg <- setRefClass("linreg",
                                   varcoeff = "matrix",
                                   tstat = "matrix",
                                   pv = "matrix"),
+                      
                       methods = list(
                         initialize = function(formula, data){
                           .self$formula <- formula
                           .self$data <- data
                           X <- model.matrix(formula, data)
-                          #formulanames <- all.vars(formula)
                           y <- as.matrix(data[all.vars(formula)[1]])#data[,which(names(data) == formulanames[1])]
                           .self$bhat <-  solve(t(X) %*% X) %*% t(X) %*% y
                           .self$yhat <- X %*% .self$bhat
                           .self$resid <- y - .self$yhat
-                          .self$df <- length(data)-length(X) #dim(data)[1]-dim(X)[2]
-                          .self$rv1 <- (t(.self$resid)%*%.self$resid)/.self$df
+                          .self$df <- nrow(data)-ncol(X) #dim(data)[1]-dim(X)[2]
+                          .self$rv1 <- as.integer((t(.self$resid)%*%.self$resid)/.self$df)
                           .self$varcoeff <-(.self$rv1)*solve(t(X)%*%X)#as.numeric(.self$rv1)*solve(t(X)%*%X)
-                          .self$tstat <- .self$bhat / sqrt(.self$varcoeff)#.self$bhat / sqrt(diag(.self$varcoeff))
+                          .self$tstat <- .self$bhat / sqrt(diag(.self$varcoeff))#.self$bhat / sqrt(diag(.self$varcoeff))
                           .self$pv <- pt(.self$bhat, .self$df)
                         },
                         
-
-                          
-  )
-
-print <- function(formula, data){
-  lin <- linreg$new(formula, data)
-  return(t(lin$bhat()))
-}
-
-plot <- function(formula, data){
-  lin <- linreg$new(formula, data)
-  a <- lin$resid()
-  b <- lin$yhat()
-  
-  c <- as.data.frame(cbind(a,b))
-  p <-  ggplot(c, aes(x = c[,2], y = c[,1]))  + geom_point(pch = 1, size = 4) +
-    geom_smooth(linetype = 3, colour = "black", method = "lm",se = FALSE)+
-    geom_smooth(linetype = 1, colour = "black", method = "loess",se = FALSE)+theme_bw()
-  p <- p +  xlab("Fitted values") +
-    ylab("Residuals") +
-    ggtitle("Residuals vs Fitted")
-  d <- sqrt(abs(lin$resid()))
-  e <- as.data.frame(cbind(d,b))
-  g <-  ggplot(e, aes(x = e[,2], y = e[,1]))  + geom_point(pch = 1, size = 4) +
-    geom_smooth(method='loess', se = FALSE) +
-    theme_bw()
-  g <- g +  xlab("Fitted values") +
-    ylab(label = expression(paste(sqrt("|Standardized residuals|")))) +
-    ggtitle(label = "Scale-Location")
-}
-
-
-resid <- function(formula, data){
-  lin <- linreg$new(formula, data)
-  return(lin$resid())
-}
-
-pred <- function(formula, data){
-  lin <- linreg$new(formula, data)
-  return(lin$yhat())
-}
-head(pred(Petal.Length~Species, data = iris))
-pred(Petal.Length~Species, data = iris)
-
-coef <- function(formula, data){
-  lin <- linreg$new(formula, data)
-  return(names(lin$bhat()))
-}
-coef(Petal.Length~Species, data = iris)
-
-summary <- function(formula, data){
-  lin <- linreg$new(formula, data = data)
-  list( bhat=lin$bhat(),
-        resid=lin$resid(),
-        varcoef=lin$varcoeff(),
-        tstat=lin$tstat(),
-        pv=lin$pv(),
-        rv1=lin$rv1(),
-        df=lin$df()
-  )
-}
-
-
-
+                        print = function(){
+                          return(.self$bhat)
+                        },
+                        
+                        plot = function(){
+                          sq_resid <- sqrt(abs(.self$resid))
+                          z <- data.frame( .self$resid,  .self$yhat, sq_resid)
+                          colnames(z) <- c("a1", "a2", "a3")
+                          p <- ggplot(z, aes(a2, a1))  + geom_point(pch = 1, size = 4) +
+                            geom_smooth(linetype = 3, colour = "black", method = "lm",se = FALSE)+
+                            geom_smooth(linetype = 1, colour = "black", method = "loess",se = FALSE)+theme_bw()
+                          p <- p +  xlab("Fitted values") +
+                            ylab("Residuals") +
+                            ggtitle("Residuals vs Fitted")
+                          g <- ggplot(z, aes(x = a2, y = a3))  + geom_point(pch = 1, size = 4) +
+                            geom_smooth(linetype = 1, colour = "black", method='loess', se = FALSE) +
+                            theme_bw()
+                          g <- g +  xlab("Fitted values") +
+                            ylab(label = expression(paste(sqrt("|Standardized residuals|")))) +
+                            ggtitle(label = "Scale-Location")
+                           return(grid.arrange(p, g, nrow = 2))
+                        },
+                        
+                        resid = function(){
+                          return(.self$resid)
+                        },
+                        
+                        pred = function(){
+                          return(.self$yhat)
+                        },
+                        
+                        summary = function(){
+                          summ <- list( bhat=.self$bhat,
+                                        resid=.self$resid,
+                                        varcoef=.self$varcoeff,
+                                        tstat=.self$tstat,
+                                        pv=.self$pv,
+                                        rv1=.self$rv1,
+                                        df=.self$df)
+                          return(summ)
+                        },
+                        a = function(){
+                          z <- data.frame(.self$resid, .self$yhat)
+                          return(z[,1])
+                        }
+                      )
+)
